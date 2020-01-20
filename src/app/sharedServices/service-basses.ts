7202c7fa5b0cc007e281762104b889b35704fd48
Luis Sanchez <lsancheza@vectoritcgroup.com>
2020-01-20 19:47:07 +0100

    basic guitars and bass pages

--- src/app/sharedServices/service-basses.ts
+++ src/app/sharedServices/service-basses.ts
@@ -1,18 +1,21 @@
 export class ServiceBasses {
     private readonly basses:object[]=[
         {
+            id:0,
             name:"Bass Les Paul",
             price:1650,
             woodType:"Alce",
             clavijeroType:"Doble arco"
         },
         {
+            id:1,
             name:"Bass Stratocaster",
             price:1650,
             woodType:"Alce",
             clavijeroType:"Doble arco"
         },
         {
+            id:2,
             name:"Bass Fender ST",
             price:1650,
             woodType:"Alce",
--- src/app/sharedServices/service-guitars.ts
+++ src/app/sharedServices/service-guitars.ts
@@ -1,18 +1,21 @@
 export class ServiceGuitars {
 private readonly guitars:object[]=[
     {
+        id:0,
         name:"Les Paul",
         price:1650,
         woodType:"Alce",
         clavijeroType:"Doble arco"
     },
     {
+        id:1,
         name:"Stratocaster",
         price:1650,
         woodType:"Alce",
         clavijeroType:"Doble arco"
     },
     {
+        id:2,
         name:"Fender ST",
         price:1650,
         woodType:"Alce",
