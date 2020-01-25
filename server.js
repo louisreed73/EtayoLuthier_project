f9f19a9220c1831cec99708443e659fe4516055d
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-01-25 15:16:32 +0100

    cambio config server.js and packckage.json node server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-// app.use(express.static(__dirname + '/dist/Angular10'));
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/Angular10'));
+// app.use(express.static(__dirname + '/src'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    // res.sendFile(path.join(__dirname + '/dist/Angular10/index.html'));
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Angular10/index.html'));
+    // res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // default Heroku PORT
