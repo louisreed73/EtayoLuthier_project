50dd9a2befdff9fb9b5d2ce2a6cb4ac97d933750
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-01-25 11:57:37 +0100

    renombrado app

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/etayo-luthier'));
+app.use(express.static(__dirname + '/dist/Angular10'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/etayo-luthier/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Angular10/index.html'));
 });
 
 // default Heroku PORT
