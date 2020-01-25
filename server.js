bef7794359ecbd7687df53e91af742d2478c7482
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-01-25 12:56:24 +0100

    cambio server.js alternativa development

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Angular10'));
+// app.use(express.static(__dirname + '/dist/Angular10'));
+app.use(express.static(__dirname + '/src));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/Angular10/index.html'));
+    // res.sendFile(path.join(__dirname + '/dist/Angular10/index.html'));
+    res.sendFile(path.join(__dirname + '/src'));
 });
 
 // default Heroku PORT
