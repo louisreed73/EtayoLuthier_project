1c340c9b4b9c8a781ef5cfe689b5dbf72488e0a2
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-01-25 12:04:46 +0100

    cambio guitar y bass component de object to any type from servicio

--- src/app/components/bass/bass.component.ts
+++ src/app/components/bass/bass.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BassComponent implements OnInit {
 
-  @Input() bass:object;
+  @Input() bass:any;
 
   constructor() { }
 
--- src/app/components/guitar/guitar.component.ts
+++ src/app/components/guitar/guitar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GuitarComponent implements OnInit {
 
-  @Input() guitar:object;
+  @Input() guitar:any;
 
   constructor() { }
 
