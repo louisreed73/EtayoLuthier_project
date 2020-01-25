0f0b00410f1e7aa91798e31f8daf22f28618e2f7
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-01-25 11:45:42 +0100

    despliegue HEROKU3

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Etayo_Luthier'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/Etayo_Luthier/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
