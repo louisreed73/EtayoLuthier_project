8a624ee27076e4e576cae1b85f45b60eda0d656d
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-04-07 22:37:01 +0200

    configuracion header responsive

--- src/app/components/header1/header1.component.ts
+++ src/app/components/header1/header1.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+const {log:l}=console;
+
 @Component({
-  selector: 'app-header1',
-  templateUrl: './header1.component.html',
-  styleUrls: ['./header1.component.scss']
+  selector: "app-header1",
+  templateUrl: "./header1.component.html",
+  styleUrls: ["./header1.component.scss"],
 })
 export class Header1Component implements OnInit {
+  isDeskTop = false;
+  wW = window.innerWidth;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+       this.calculoW();
+
   }
 
+  onResize(e: Event) {
+    this.wW = (e.target as Window).innerWidth;
+    this.calculoW()
+
+
+  }
+
+  calculoW() {
+        if (this.wW >= 678) {
+          this.isDeskTop = true;
+        } else {
+          this.isDeskTop = false;
+        }
+        l("Cambiando tamaño window!!!!", this.wW);
+
+  }
 }
